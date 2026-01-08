f2931c7721086eb0311122e68ced8e8a6bcb601e
Yash Kanwar <kanwarzen642@gmail.com>
2026-01-08 10:00:01 +0530

    dynamic routing implemented

--- src/contollers/posts.controller.js
+++ src/contollers/posts.controller.js
@@ -2,4 +2,9 @@ const getallposts=(req,res)=>{
     const posts=[{data1:"post1"},{data2:"post2"}]
     res.json(posts)
 }
-module.exports={getallposts}
+async function getuserID(req,res){
+    const userID=req.params.id
+    res.status(200).json({message:`Fetching data for post with ID:${userID}`})
+}
+module.exports={getallposts,getuserID}
+
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,4 +2,5 @@ express=require('express');
 const router=express.Router()
 const postcontroller=require('../contollers/posts.controller')
 router.get('/',postcontroller.getallposts)
+router.get('/:id',postcontroller.getuserID)
 module.exports=router
