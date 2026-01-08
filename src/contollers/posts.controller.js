a33e414e3b65ff2fabebb6f43237a64f807a839d
Yash Kanwar <kanwarzen642@gmail.com>
2026-01-08 16:05:59 +0530

    ADDED FORMATED JSON DATA

--- src/contollers/posts.controller.js
+++ src/contollers/posts.controller.js
@@ -1,10 +1,21 @@
 const getallposts=(req,res)=>{
-    const posts=[{data1:"post1"},{data2:"post2"}]
-    res.json(posts)
+    const {sortby} = req.query;
+    const posts=[{data1:"post1",date:"2023-01-01"},{data2:"post2",date:"2023-01-02"}]
+    if(sortby==="date"){
+        posts.sort((a,b)=>new Date(b.date)-Date(a.date))
+        console.log("posts sorted by date")
+    }
+    res.status(200).json({
+  success: true,
+  message: 'All posts fetched successfully'
+});
 }
 async function getuserID(req,res){
     const userID=req.params.id
-    res.status(200).json({message:`Fetching data for post with ID:${userID}`})
+   res.status(200).json({
+  success: true,
+  data: { postId: userID }
+});
 }
 module.exports={getallposts,getuserID}
 
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,4 +3,8 @@ const router=express.Router()
 const postcontroller=require('../contollers/posts.controller')
 router.get('/',postcontroller.getallposts)
 router.get('/:id',postcontroller.getuserID)
+router.post('/test',(req,res)=>{
+    console.log("request recieved")
+    res.status(200).json({message:`post created: ${req.body.title} `})
+})
 module.exports=router
